0ea4a9c9b9965df7464f1bd1a349e0e5aff5ae74
conto <constantijn@vanhartesveldt.nl>
2021-11-26 14:54:56 +0100

    working

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
 // exports.Inserts = require("./lib/Inserts.js");
-// exports.DatabaseCore = require("./lib/DatabaseCore");
+// exports.DatabaseCore = require("./lib/DatabaseCore")
+require("dotenv").config({
+    path: "../.env",
+});
 
+const Bulk = require("./lib/Bulk");
 const Inserts = require("./lib/Inserts");
 const Update = require("./lib/Update")
 
-class DatabaseCore{
-    constructor(){
+class DatabaseCore {
+    Database
+    MongoUrl
+    constructor(Database,MongoUrl) {
         console.log("database core loaded");
 
+        // init for mongo db 
+        this.Database = Database
+        this.MongoUrl = MongoUrl
+
+        // loads all the sub classes for the package
         this.Inserts = new Inserts()
-
         this.Update = new Update()
+        this.Bulk = new Bulk()
     }
-    function kaas() {
+
+    kaas() {
         const MongoClient = require("mongodb").MongoClient;
-// Connection URI
-require("dotenv").config({
-  path: "../.env",
-});
-const uri = process.env.DB_URL;
-const client = new MongoClient(uri, {
-  useUnifiedTopology: true,
-});
+        // Connection URI
+        const uri = process.env.DB_URL;
+        const client = new MongoClient(uri, {
+            useUnifiedTopology: true,
+        });
 
-const connection = client.connect();
-const connect = connection;
+        const connection = client.connect();
+        // const connect = connection;
 
-const dbo = client.db("discord");
-
+        const dbo = client.db("discord");
+        return connection
     }
-    
+
 }
 
 module.exports = DatabaseCore
--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,6 @@
 const easyMongoDB = require("./index.js")
 
-const DB = new easyMongoDB()
+const DB = new easyMongoDB("database","url")
 
 DB.Inserts.one()
 DB.Update.one()
